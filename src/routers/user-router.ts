c1d65eb4988c3a760e9f52f1a430f3b9ac025dce
Analice <anadias.alice@gmail.com>
2022-12-15 11:54:20 -0300

    feat: post /singIn

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -1,11 +1,13 @@
-import { singUP } from "../controllers/user-controller";
+import { singIn, singUp } from "../controllers/user-controller";
 import schemaValidator from "../middlewares/schemaValidator-middleware";
-import { createUserSchema } from "../schemas/user-schema";
+import { createUserSchema, singInSchema } from "../schemas/user-schema";
 import { Router } from "express";
 import { checkIfUserExists } from "../middlewares/user-middleware";
 
 const usersRouter = Router();
 
-usersRouter.post("/singUp", schemaValidator(createUserSchema), checkIfUserExists, singUP)
+usersRouter
+    .post("/singUp", schemaValidator(createUserSchema), checkIfUserExists, singUp)
+    .post("/singIn", schemaValidator(singInSchema), singIn)
 
 export { usersRouter };
