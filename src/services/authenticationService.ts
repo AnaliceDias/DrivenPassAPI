f528cf0b57a2dafa1983d4b89fcd600b89e690d7
Analice <anadias.alice@gmail.com>
2022-12-15 11:49:47 -0300

    refactor

--- src/services/authenticationService.ts
+++ src/services/authenticationService.ts
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function generateToken(userId: number) {
     const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET);
