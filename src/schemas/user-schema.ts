274f534f070e5e3d5a1f3cd53e68b25b50836242
Analice <anadias.alice@gmail.com>
2022-12-15 11:42:06 -0300

    feat: singInSchema

--- src/schemas/user-schema.ts
+++ src/schemas/user-schema.ts
@@ -4,3 +4,8 @@ export const createUserSchema = joi.object({
   email: joi.string().email().required(),
   password: joi.string().min(10).required(),
 });
+
+export const singInSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
