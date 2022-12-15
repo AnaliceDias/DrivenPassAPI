ae37647462435b6b9ec419bef58edbb69bc319af
Analice <anadias.alice@gmail.com>
2022-12-15 12:09:58 -0300

    feat: tokenValidator middleware

---
+++ src/middlewares/authorization-middleware.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import {Request, Response, NextFunction} from "express"
+import httpStatus from "http-status";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default function tokenValidator(req: Request, res: Response, next: NextFunction) {
+  const { authorization } = req.headers;
+
+  const token = authorization?.replace("Bearer", "").trim();
+  if (!token) {
+    return res.status(httpStatus.UNAUTHORIZED).send("Token not found");
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (e) {
+    res.status(httpStatus.UNAUTHORIZED).send("Token has expired");
+  }
+}
